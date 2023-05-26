a80ecffe41edbd56df784cbbbfb26ff7a30b37bd
Jaime Tavares <tavaresjaime00@gmail.com>
2023-05-25 20:29:32 -0400

    Initial Commit

---
+++ tech-wordle/tech_wordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  tech_wordleApp.swift
+//  tech-wordle
+//
+//  Created by Jaime Tavares on 2023-05-25.
+//
+
+import SwiftUI
+
+@main
+struct tech_wordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
