ba1b879de2039e5b2452b3505eb8ef5a5aeaf0a5
Jaime Tavares <tavaresjaime00@gmail.com>
2023-05-27 05:50:58 -0400

    - Added a Statistics view, to see how you are doing over time. - The game will now save the user's state upon closing the app, and loaded when re-opening again. - Animations added for correct and incorrect words. - Keyboard-letters will change colour depending on if that letter is in the word or not. - Upon winning, or running out of guesses, a pop-up appears to show the results.

--- tech-wordle/ContentView.swift
+++ tech-wordle/ContentView.swift
@@ -8,28 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-  @StateObject var game = GuessingGame()
-  @State private var showStats = false
+    
+    @Environment(\.scenePhase) var scenePhase
+    @StateObject var game = GuessingGame()
+    @State private var showStats = false
+    @State private var showResults = false
 
-  var body: some View {
-    VStack {
-      Text("Jaime's Words")
-            .font(.title)
-            .accessibilityAddTraits(.isHeader)
-            .foregroundColor(.green)
-      GameBoardView(game: game)
-      KeyboardView(game: game)
-        .padding(5)
-        ActionBarView(showStats: $showStats, game: game)
+    var body: some View {
+        VStack {
+            Text("Jay's Words")
+                .font(.title)
+                .accessibilityAddTraits(.isHeader)
+                .foregroundColor(.green)
+            GameBoardView(game: game)
+            KeyboardView(game: game)
+                .padding(5)
+            ActionBarView(showStats: $showStats, game: game)
+        }
+        .sheet(isPresented: $showResults) {
+            GameResultView(game: game).background(.black)
+        }
+        .sheet(isPresented: $showStats) {
+            StatisticsView(stats: GameStatistics(gameRecord: game.gameRecord)).background(.black)
+        }
+        .onChange(of: game.status) { newStatus in
+            if newStatus == .won || newStatus == .lost {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                    showResults = true
+                }
+            }
+        }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .active {
+                if game.status == .new && !game.gameState.isEmpty {
+                    game.loadState()
+                }
+            }
+            if newPhase == .background || newPhase == .inactive {
+                game.saveState()
+            }
+        }
+        .frame(alignment: .top)
+        .padding([.bottom], 10)
+        .background(.black)
+        }
     }
-    .frame(alignment: .top)
-    .padding([.bottom], 10)
-    .background(.black)
-  }
-}
 
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
-}
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
--- tech-wordle/Views/CurrentGuessView.swift
+++ tech-wordle/Views/CurrentGuessView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct CurrentGuessView: View {
-    
+    @State var shakeOffset = 0.0
     @Binding var guess: Guess
     var wordLength: Int
     
@@ -25,6 +25,9 @@ struct CurrentGuessView: View {
                 ForEach(guess.word.indices, id: \.self) { index in
                     let letter = guess.word[index]
                     GuessBoxView(letter: letter, size: width, index: index)
+                        .accessibilityLabel(
+                            letter.status == .unknown ? letter.letter : "\(letter.letter) \(letter.status.rawValue)"
+                        )
                 }
                 ForEach(0..<unguessedLetters, id: \.self) { _ in
                     EmptyBoxView(size: width)
@@ -33,6 +36,23 @@ struct CurrentGuessView: View {
                 Spacer()
             }
             .padding(5.0)
+            .offset(x: shakeOffset)
+            .onChange(of: guess.status) { newValue in
+                if newValue == .invalidWord {
+                    withAnimation(.linear(duration: 0.1).repeatCount(3)) {
+                        shakeOffset = -15.0
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                        withAnimation(.linear(duration: 0.1).repeatCount(3)) {
+                            shakeOffset = 0.0
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                                guess.status = .pending
+                            }
+                        }
+                    }
+                }
+            }
             .overlay(
                 Group {
                     if guess.status == .invalidWord {
