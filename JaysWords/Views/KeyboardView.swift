2e243253f2adffebb312cfd22fc8c15e43cd8a9e
Jaime Tavares <tavaresjaime00@gmail.com>
2023-06-10 00:42:22 -0400

    Fixed dynamic sizing of keyboard-button elements for all device sizes.

--- JaysWords/Views/KeyboardView.swift
+++ JaysWords/Views/KeyboardView.swift
@@ -8,32 +8,34 @@
 import SwiftUI
 
 struct KeyboardView: View {
-  @ObservedObject var game: GuessingGame
-  let keyboard = "QWERTYUIOP|ASDFGHJKL|<ZXCVBNM>"
-
-  var body: some View {
-    let lines = keyboard.split(separator: "|")
-    VStack {
-      ForEach(lines, id: \.self) { line in
-        HStack {
-          let keyArray = line.map { String($0) }
-            ForEach(keyArray, id: \.self) { key in
-
-              
-                KeyButtonView(game: game, key: key)
-                    .accessibilityLabel(
-                        game.statusForLetter(letter: key) == .unknown ? key : "\(key) \(game.statusForLetter(letter: key).rawValue)"
-                    )
+    @ScaledMetric var dynamicSize: CGFloat = 1
+    
+    @ObservedObject var game: GuessingGame
+    let keyboard = "QWERTYUIOP|ASDFGHJKL|<ZXCVBNM>"
+    
+    var body: some View {
+        let lines = keyboard.split(separator: "|")
+        VStack {
+            ForEach(lines, id: \.self) { line in
+                HStack {
+                    let keyArray = line.map { String($0) }
+                    ForEach(keyArray, id: \.self) { key in
+                        
+                        
+                        KeyButtonView(game: game, key: key)
+                            .accessibilityLabel(
+                                game.statusForLetter(letter: key) == .unknown ? key : "\(key) \(game.statusForLetter(letter: key).rawValue)"
+                            )
+                    }
+                    
+                }.padding(4)
             }
-            
-        }.padding(5)
-      }
+        }
     }
-  }
 }
 
 struct KeyboardView_Previews: PreviewProvider {
-  static var previews: some View {
-    KeyboardView(game: GuessingGame())
-  }
+    static var previews: some View {
+        KeyboardView(game: GuessingGame())
+    }
 }
